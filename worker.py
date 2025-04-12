5f9d0ca5609d0eff62abb6471f1fa72cc1ab637b
noahfrank3 <noahfrank3@outlookc.om>
2025-04-12 15:46:59 -0400

    Add environment variable for number of workers

--- worker.py
+++ worker.py
@@ -1,4 +1,7 @@
+import os
 from dask.distributed import Worker
+
+num_workers = os.getenv('NUM_WORKERS')
 
 # Replace with the actual public IP of your scheduler
 SCHEDULER_ADDRESS = "tls://<scheduler-IP>:8786"
