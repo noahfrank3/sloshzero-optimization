193ad4d180e5456dd2ebd9986b48cabf8653dd2d
noahfrank3 <noahfrank3@outlookc.om>
2025-04-14 04:05:23 -0400

    Fix scheduler closing early

--- dask_scheduler/scheduler.py
+++ dask_scheduler/scheduler.py
@@ -1,8 +1,12 @@
+import asyncio
 import os
 
-from dask.distributed import Scheduler  
+from dask.distributed import Scheduler
 
-port = int(os.environ.get("PORT", 8786))
-scheduler = Scheduler(port=port)  
-scheduler.start()
-scheduler.sync(loop=True)
+async def start_scheduler():
+    port = int(os.environ.get("PORT", 8786))
+    scheduler = Scheduler(port=port)
+    await scheduler.start()
+    await asyncio.Event().wait()
+
+asyncio.run(start_scheduler())
