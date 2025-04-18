4fa77cbcb85075c5fefe3d761630680232aecb5a
noahfrank3 <noahfrank3@outlookc.om>
2025-04-18 08:15:11 -0400

    Rename directory

--- sloshzero/worker.py
+++ sloshzero/worker.py
@@ -1,8 +1,6 @@
-from distributed import get_worker
-
-from F_slosh import F_slosh
-
 def get_F_slosh(params):
+    exec('from F_slosh import F_slosh')
+
     # Unpack parameters
     '''
     x_1 = params['x_1']
@@ -16,6 +14,3 @@ def get_F_slosh(params):
     F_slosh_val = F_slosh(params)
     # F_slosh_val = F_slosh(x_1, x_2, x_3, d_pt, s_d)
     return F_slosh_val
-
-worker = get_worker()
-worker.handlers['F_slosh'] = get_F_slosh
